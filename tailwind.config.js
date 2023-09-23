004b35438790288c11093c17993e29c3e86bb757
Marte <marte.mk@hotmail.com>
2023-09-23 10:19:05 +0000

    Setup project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./feed/**/*.{html,js}",
+    "./profile/**/*.{html,js}",
+    "./search/**/*.{html,js}",
+    "./src/**/*.{html,js}",
+    "./index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
