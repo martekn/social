20eba41c31020d2ffc4a062c7984665fe2294c4e
Marte <marte.mk@hotmail.com>
2023-09-29 09:59:32 +0000

    Setup base styling and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,53 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    container: {
+      center: true,
+    },
+    colors: {
+      transparent: "transparent",
+      primary: {
+        300: "#3F88A7",
+        400: "#126D94",
+        500: "#0D516E",
+        600: "#073649",
+      },
+      dark: {
+        200: "#808093",
+        300: "#5E5E6E",
+        400: "#4F5263",
+        500: "#0D1026",
+      },
+      light: {
+        200: "#fff",
+        300: "#fcfcfd",
+        400: "#F3F4F7",
+        500: "#dce0ec",
+      },
+    },
+    fontFamily: {
+      base: [
+        '"Open sans"',
+        "ui-sans-serif",
+        "system-ui",
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      accent: [
+        "Montserrat",
+        '"Open sans"',
+        "ui-sans-serif",
+        "system-ui",
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+    },
   },
   plugins: [],
 };
