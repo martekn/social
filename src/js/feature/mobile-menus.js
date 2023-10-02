8b4391ff089765d77514997d2257298113f982d7
Marte <marte.mk@hotmail.com>
2023-10-02 17:24:20 +0000

    setup the mobile menus (nav and sidebar)

--- src/js/feature/mobile-menus.js
+++ src/js/feature/mobile-menus.js
@@ -1,6 +1,12 @@
 import { stringToBoolean } from "../helper/string-to-boolean.js";
+import { handleFocusTrap } from "../helper/handle-focus-trap.js";
 
-const mobileMenuToggle = function (button) {
+const navButton = document.querySelector("#nav-button");
+const navClose = document.querySelector("#nav-close");
+const sidebarButton = document.querySelector("#sidebar-button");
+const sidebarClose = document.querySelector("#sidebar-close");
+
+const mobileMenuToggle = (button) => {
   const menuId = button.getAttribute("aria-controls");
   const menu = document.querySelector(`#${menuId}`);
   let isVisible = stringToBoolean(menu.getAttribute("data-mobile-visible"));
@@ -10,25 +16,44 @@ const mobileMenuToggle = function (button) {
   menu.setAttribute("data-mobile-visible", isVisible);
 };
 
-export const setupMobileMenus = () => {
-  const navButton = document.querySelector("#nav-button");
-  const navClose = document.querySelector("#nav-close");
-  const sidebarButton = document.querySelector("#sidebar-button");
-  const sidebarClose = document.querySelector("#sidebar-close");
+const handleFocusNav = (e) => {
+  const nav = document.querySelector(
+    `#${navButton.getAttribute("aria-controls")}`,
+  );
 
+  handleFocusTrap(nav, "button, a", closeNav, e);
+};
+
+const closeNav = (e) => {
+  document.removeEventListener("keydown", handleFocusNav);
+  mobileMenuToggle(navButton);
+};
+
+const handleFocusSidebar = (e) => {
+  const sidebar = document.querySelector(
+    `#${sidebarButton.getAttribute("aria-controls")}`,
+  );
+
+  handleFocusTrap(sidebar, "button, a, input", closeSidebar, e);
+};
+
+const closeSidebar = (e) => {
+  mobileMenuToggle(sidebarButton);
+  document.removeEventListener("keydown", handleFocusSidebar);
+};
+
+export const setupMobileMenus = () => {
   navButton.addEventListener("click", (e) => {
     mobileMenuToggle(navButton);
+    document.addEventListener("keydown", handleFocusNav);
   });
 
-  navClose.addEventListener("click", (e) => {
-    mobileMenuToggle(navButton);
-  });
+  navClose.addEventListener("click", closeNav);
 
   sidebarButton.addEventListener("click", (e) => {
     mobileMenuToggle(sidebarButton);
+    document.addEventListener("keydown", handleFocusSidebar);
   });
 
-  sidebarClose.addEventListener("click", (e) => {
-    mobileMenuToggle(sidebarButton);
-  });
+  sidebarClose.addEventListener("click", closeSidebar);
 };
