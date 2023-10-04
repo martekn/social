6d251bc693d34c94583f3c246ac391ef64a909ba
Marte <marte.mk@hotmail.com>
2023-10-04 09:23:34 +0000

    Create action button components html and styling for the posts

---
+++ src/js/components/action-button.js
@@ -0,0 +1,37 @@
+import htmlUtilities from "../helper/html-utilities/index.js";
+
+class PostAction extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    let action;
+    const actionClasses =
+      "p-1 hover:bg-light-450 w-full flex hover:text-dark-500 align-middle gap-2 justify-center";
+
+    if (this.getAttribute("type") === "link") {
+      action = htmlUtilities.createHTML("a", actionClasses, null, {
+        href: this.getAttribute("href"),
+      });
+    } else {
+      action = htmlUtilities.createHTML("button", actionClasses);
+    }
+    const icon = htmlUtilities.createHTML("i", this.getAttribute("icon"));
+    const buttonText = htmlUtilities.createHTML(
+      "span",
+      "sr-only sm:not-sr-only",
+      this.textContent,
+    );
+    this.textContent = "";
+    action.append(...[icon, buttonText]);
+
+    this.appendChild(action);
+  }
+}
+
+customElements.define("post-action", PostAction);
--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,4 @@ import "./components/search-bar.js";
 import "./components/nav-item.js";
 import "./components/tag-item.js";
 import "./components/username-item.js";
+import "./components/action-button.js";
