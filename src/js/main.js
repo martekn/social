774871ff96bb15bbf3dbf55d13b16e352b617902
Marte <marte.mk@hotmail.com>
2023-10-06 08:35:22 +0000

    Update import paths after folder restructure

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 import "./components/search-bar.js";
-import "./components/nav-item.js";
+import "./components/navigation/nav-item.js";
 import "./components/tag-item.js";
 import "./components/username-item.js";
-import "./components/action-button.js";
-import "./components/post-item.js";
+import "./components/post/post-item.js";
 import "./components/filter-button.js";
