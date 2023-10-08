68bafda9c6231a1c9b425aefdd5d1ae11bb50eae
Marte <marte.mk@hotmail.com>
2023-10-08 16:33:36 +0000

    Update AppSidebar and AppNavigation imports

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -2,6 +2,6 @@ import "./search-bar.js";
 import "./tag-item.js";
 import "./post/post-item.js";
 import "./filter-button.js";
-import "./navigation-component.js";
-import "./sidebar-component.js";
+import "./app-navigation.js";
+import "./app-sidebar.js";
 import "./user-badge";
