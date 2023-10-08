29d926f063b11afa82a6699b4741e347d51a7042
Marte <marte.mk@hotmail.com>
2023-10-08 11:03:26 +0000

    Create file to handle component imports

---
+++ src/js/components/index.js
@@ -0,0 +1,7 @@
+import "./search-bar.js";
+import "./tag-item.js";
+import "./post/post-item.js";
+import "./filter-button.js";
+import "./navigation-component.js";
+import "./sidebar-component.js";
+import "./username-item.js";
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,1 @@
-import "./components/search-bar.js";
-import "./components/tag-item.js";
-import "./components/post/post-item.js";
-import "./components/filter-button.js";
-import "./components/navigation-component.js";
-import "./components/sidebar-component.js";
-import "./components/username-item.js";
+import "./components/index.js";
