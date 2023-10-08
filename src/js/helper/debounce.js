7e1fd1bcc8cc34ef75a75fdcf9265400ae21e583
Marte <marte.mk@hotmail.com>
2023-10-08 10:23:35 +0000

    Add debounce helper function

---
+++ src/js/helper/debounce.js
@@ -0,0 +1,25 @@
+/**
+  * Creates a debounced function that delays invoking the provided function
+ * until a specified amount of time has passed since the last time it was invoked.
+ *
+ * @param {Function} func - The function to be debounced.
+ * @param {number} [timeout=150] - The time in milliseconds to wait before invoking `func`.
+ * @returns {Function} A debounced version of the provided function.
+ * @example
+ * ```js
+ * window.addEventListener(
+  "resize",
+  debounce(function (e) {
+    alert("end of resizing");
+  }),
+);
+
+ * ```
+ */
+const debounce = (func, timeout = 150) => {
+  let timer;
+  return function (e) {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(func, timeout, e);
+  };
+};
