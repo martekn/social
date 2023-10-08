526b90a5e08f422e066ea009c7c3b2eef12fc36f
Marte <marte.mk@hotmail.com>
2023-10-08 16:20:27 +0000

    Refactor and update AppSidebar component
    - Refactor and clean up code
    - Update the html to reflect changes
    - Add component class 'sidebar'

--- src/js/components/app-sidebar.js
+++ src/js/components/app-sidebar.js
@@ -1,26 +1,21 @@
 import htmlUtilities from "../helper/html-utilities/index.js";
-import { tags } from "../const/test-data/tags.js";
-import { following } from "../const/test-data/following.js";
 import { handleFocusTrap } from "../helper/handle-focus-trap.js";
 import { mobileMenuToggle } from "../helper/mobile-menu-toggle.js";
+import { TagItem } from "./tag-item.js";
+import { UserBadge } from "./user-badge.js";
+import { SearchBar } from "./search-bar.js";
 
 const sidebarButton = document.querySelector("#sidebar-button");
 
-/**
- * The component has to be inserted into an aside element with existing classes
- * If not the layout will shift quite heavily on load
- * @example
- * ```html
- *  <aside id="sidebar" data-mobile-visible="false" class="inset-y-0 right-0 z-50 order-2 hidden w-full max-w-md bg-light-200 p-7 pt-2 shadow-xl    transition-all data-[mobile-visible='true']:absolute data-[mobile-visible='true']:block data-[mobile-visible='true']:animate-slide md:pt-7 lg:col-span-3 lg:block lg:bg-light-400 lg:px-10 lg:shadow-none lg:data-[mobile-visible='true']:static" >
-      <sidebar-component></sidebar-component>
-    </aside>
- * ```
- */
-class SidebarComponent extends HTMLElement {
+// * Add the class 'sidebar' to the sidebar where its used in the html to avoid the layout from shifting
+class AppSidebar extends HTMLElement {
+  static get observedAttributes() {
+    return ["tags-loaded", "following-loaded"];
+  }
   constructor() {
     super();
-    this.following = following;
-    this.tags = tags;
+    this.following = [];
+    this.tags = [];
     this.isSearchpage = window.location.pathname === "/search/";
   }
 
@@ -36,6 +31,16 @@ class SidebarComponent extends HTMLElement {
     sidebarClose.addEventListener("click", this.closeSidebar);
   }
 
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "tags-loaded" && newValue === "true") {
+      this.renderTags(this.tags);
+    }
+
+    if (name === "following-loaded" && newValue === "true") {
+      this.renderFollowing(this.following);
+    }
+  }
+
   /**
    * Toggles menu visibility and removes eventhandler
    * @param {MouseEvent} e - Event from click
@@ -46,9 +51,8 @@ class SidebarComponent extends HTMLElement {
   }
 
   /**
-   * Callback function for event
-   * Setup for focus trap
-   * @param {MouseEvent} e - Event from click eventlistener
+   * Handles focus trapping in the sidebar menu.
+   * @param {MouseEvent} e - Click event.
    */
   // * The method has to be an arrow function, because of the context of the 'this' keyword in relation to class and eventlistener
   // * In order to preserve the 'this' context the callback function also need to use .bind(this)
@@ -67,100 +71,44 @@ class SidebarComponent extends HTMLElement {
   };
 
   /**
-   * Creates username element
-   * @param {{name, avatar}} user - User information
-   * @returns Username element with avatar and name
+   * Renders a section element for the sidebar.
+   * @param {String} text - Heading text.
+   * @param {String} listId - ID for the list element.
+   * @returns {HTMLElement} - Section element containing heading and ul.
    */
-  renderUsername(user) {
-    const username = htmlUtilities.createHTML(
-      "a",
-      "flex items-center link link-secondary gap-2 py-1",
-      null,
-      {
-        href: `/profile/?u=${user.name}`,
-      },
-    );
-
-    const avatar = htmlUtilities.createHTML(
-      "img",
-      "aspect-square w-11 rounded-full object-cover",
-      null,
-      {
-        src: user.avatar
-          ? user.avatar
-          : "/assets/images/avatar-placeholder.jpg",
-        alt: user.name,
-      },
-    );
-
-    const name = htmlUtilities.createHTML("span", null, user.name);
-
-    htmlUtilities.appendArray([avatar, name], username);
-    return username;
-  }
-
-  /**
-   * Creates section element for the sidebar
-   * @param {String} text - Text for the heading
-   * @returns Section element containing heading and ul
-   */
-  renderSection(text) {
+  renderSection(text, listId) {
     const section = htmlUtilities.createHTML("section", "py-4");
     const heading = htmlUtilities.createHTML(
       "h2",
       "font-light text-dark-300",
       text,
     );
-    const list = htmlUtilities.createHTML("ul", "space-y-2");
+    const list = htmlUtilities.createHTML("ul", "space-y-2", null, {
+      id: listId,
+    });
     section.append(...[heading, list]);
     return section;
   }
 
-  /**
-   * Creates popular tags section
-   * @returns Section element containing a list of tags
-   */
   renderTags() {
-    const section = this.renderSection("Popular tags");
-    if (this.isSearchpage) {
-      section.classList.add("border-t-0");
-    }
-    const list = section.querySelector("ul");
-
     for (const tag of this.tags) {
       const li = htmlUtilities.createHTML("li");
-      const tagItem = htmlUtilities.createHTML("tag-item", null, tag, {
-        "data-style": "secondary",
-      });
-      li.append(tagItem);
-      list.append(li);
+      li.append(new TagItem(tag, "secondary"));
+      document.querySelector("#tags-list").append(li);
     }
-    return section;
   }
 
-  /**
-   * Creates following section
-   * @returns Section element containing a list of following
-   */
   renderFollowing() {
-    const section = this.renderSection("Following");
-    const list = section.querySelector("ul");
-
     for (const user of this.following) {
       const li = htmlUtilities.createHTML("li");
-      const username = this.renderUsername(user);
+      const username = new UserBadge(user);
       li.append(username);
-      list.append(li);
+      document.querySelector("#following-list").append(li);
     }
-    return section;
   }
 
   render() {
-    const sidebar = htmlUtilities.createHTML(
-      "div",
-      "container flex flex-col overflow-y-auto h-full p-7 pt-2 md:pt-7 lg:px-10",
-    );
-
+    const sidebar = htmlUtilities.createHTML("aside", "flex flex-col");
     const closeButton = htmlUtilities.createHTML(
       "button",
       "link link-secondary ml-auto self-end text-2xl lg:hidden",
@@ -188,14 +136,18 @@ class SidebarComponent extends HTMLElement {
       "divide-y divide-light-500",
     );
 
-    const searchbar = htmlUtilities.createHTML("search-bar", "mb-4");
+    const searchbar = new SearchBar("sidebar-search");
 
     const content = htmlUtilities.createHTML(
       "div",
       "divide-y divide-light-500",
     );
-    const tagsSection = this.renderTags();
-    const followingSection = this.renderFollowing();
+    const tagsSection = this.renderSection("Popular tags", "tags-list");
+    if (this.isSearchpage) {
+      tagsSection.classList.add("border-t-0");
+    }
+
+    const followingSection = this.renderSection("Following", "following-list");
 
     const footer = htmlUtilities.createHTML(
       "footer",
@@ -220,8 +172,11 @@ class SidebarComponent extends HTMLElement {
       content.classList.add(...["lg:divide-y", "lg:divide-light-500"]);
     }
 
-    this.appendChild(sidebar);
+    this.setAttribute("id", "sidebar");
+    this.setAttribute("data-mobile-visible", "false");
+    this.classList.add("sidebar");
+    this.append(sidebar);
   }
 }
 
-customElements.define("sidebar-component", SidebarComponent);
+customElements.define("app-sidebar", AppSidebar);
