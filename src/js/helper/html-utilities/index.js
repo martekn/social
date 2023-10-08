ce9c4cfe0a87a38090e0602e28b9c949f7472e0f
Marte <marte.mk@hotmail.com>
2023-10-08 16:37:04 +0000

    Remove appendArray function

--- src/js/helper/html-utilities/index.js
+++ src/js/helper/html-utilities/index.js
@@ -1,10 +1,8 @@
-import { appendArray } from "./append-array.js";
 import { createHTML } from "./create-html.js";
 import { decode } from "./decode.js";
 import { parser } from "./parser.js";
 
 export default {
-  appendArray,
   createHTML,
   decode,
   parser,
