2a22a4ac2c8dcc32e414d103bece4c17e50b3170
Marte <marte.mk@hotmail.com>
2023-10-08 18:04:35 +0000

    Fix old component imports

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,7 +1,7 @@
 import "./search-bar.js";
 import "./tag-item.js";
-import "./post/post-item.js";
 import "./filter-button.js";
+import "./user-badge.js";
 import "./app-navigation.js";
 import "./app-sidebar.js";
-import "./user-badge";
+import "./post/social-post.js";
