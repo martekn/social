bfd89e283cd7a5cd2f05fb18d3ce68e41fecc80c
Marte <marte.mk@hotmail.com>
2023-10-10 21:11:38 +0000

    Setup post specific page and render post

--- src/js/pages/post.js
+++ src/js/pages/post.js
@@ -1,6 +1,22 @@
 import { profile } from "../const/test-data/profile.js";
 import { tags } from "../const/test-data/tags.js";
+import { posts } from "../const/test-data/posts.js";
+import { SocialPost } from "../components/post/social-post.js";
+import htmlUtilities from "../helper/html-utilities/index.js";
+
+const post = posts[2];
 
 const sidebar = document.querySelector("app-sidebar");
 sidebar.renderFollowing(profile.following);
 sidebar.renderTags(tags);
+const main = document.querySelector("main");
+const heading = htmlUtilities.createHTML(
+  "h1",
+  "mb-6",
+  `${post.author.name}'s post`,
+);
+const socialPost = new SocialPost(post, profile);
+
+main.append(...[heading, socialPost]);
+
+socialPost.showComments();
