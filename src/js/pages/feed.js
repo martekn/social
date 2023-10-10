6296186196c99addd3777864da27fc3c2b1f24c9
Marte <marte.mk@hotmail.com>
2023-10-10 20:51:47 +0000

    Update files with the new argument to renderPosts

--- src/js/pages/feed.js
+++ src/js/pages/feed.js
@@ -1,4 +1,5 @@
 import { posts } from "../const/test-data/posts.js";
 import { renderPosts } from "../helper/renderPosts.js";
+import { profile } from "../const/test-data/profile.js";
 
-renderPosts(posts, document.querySelector("#posts-list"));
+renderPosts(posts, document.querySelector("#posts-list"), profile);
