4baebec595eb6ed4a072ec46b2402b5dafe20382
Marte <marte.mk@hotmail.com>
2023-10-10 20:46:21 +0000

    Create modal component for followers and following

---
+++ src/js/components/profile/profile-modal.js
@@ -0,0 +1,70 @@
+import htmlUtilities from "../../helper/html-utilities/index.js";
+import { UserBadge } from "../user-badge.js";
+
+export class ProfileModal extends HTMLElement {
+  static get observedAttributes() {
+    return ["profile-loaded"];
+  }
+  constructor(modalType, title, users) {
+    super();
+    this.modalType = modalType;
+    this.title = title;
+    this.users = users;
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    const modal = htmlUtilities.createHTML(
+      "dialog",
+      "flex h-[90vh] max-h-[600px] max-w-md flex-col overflow-hidden p-0",
+      null,
+      {
+        id: `${this.modalType}-modal`,
+      },
+    );
+    const container = htmlUtilities.createHTML(
+      "div",
+      "flex items-center justify-between border-b border-light-450 px-6 py-2",
+    );
+    const heading = htmlUtilities.createHTML("h2", null, this.title);
+
+    const closeButton = htmlUtilities.createHTML(
+      "button",
+      "link link-secondary text-xl",
+    );
+    const buttonIcon = htmlUtilities.createHTML("i", "bi bi-x", null, {
+      "aria-hidden": "true",
+    });
+    const buttonText = htmlUtilities.createHTML(
+      "span",
+      "sr-only",
+      `Close ${this.title} modal`,
+    );
+
+    closeButton.addEventListener("click", (e) => {
+      const modal = document.querySelector(`#${this.modalType}-modal`);
+      modal.close();
+    });
+
+    closeButton.append(...[buttonIcon, buttonText]);
+    container.append(...[heading, closeButton]);
+
+    const list = htmlUtilities.createHTML(
+      "ul",
+      "scrollbar flex-1 overflow-y-auto px-6 space-y-1 py-4",
+    );
+    for (const user of this.users) {
+      const li = htmlUtilities.createHTML("li");
+      const userBadge = new UserBadge(user);
+      li.append(userBadge);
+      list.append(li);
+    }
+    modal.append(...[container, list]);
+    this.append(modal);
+  }
+}
+
+customElements.define("profile-modal", ProfileModal);
