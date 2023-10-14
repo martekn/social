62c148e974dac1c821426c781d64986102cd9896
Marte <marte.mk@hotmail.com>
2023-10-14 17:19:29 +0000

    Add login and registration feature

--- src/js/components/auth-form.js
+++ src/js/components/auth-form.js
@@ -1,5 +1,9 @@
 import htmlUtilities from "../helper/html-utilities/index.js";
 import { InputGroup } from "./input-group.js";
+import { getFormData } from "../helper/get-form-data.js";
+import { login } from "../helper/api/authRequests/login.js";
+import { register } from "../helper/api/authRequests/register.js";
+import { ErrorDialog } from "./error/error-dialog.js";
 
 /**
  * Represents an `AuthForm` class that creates a form for user registration or login based on the query of the site.
@@ -10,28 +14,46 @@ export class AuthForm extends HTMLElement {
   constructor() {
     super();
 
-    this.method = "GET";
-    this.query = new URLSearchParams(window.location.search).get("auth");
+    this.method = "POST";
+    this.isRegisterForm =
+      new URLSearchParams(window.location.search).get("auth") === "register";
 
-    if (this.query === "register") {
-      this.action = "/profile/?r=registered";
-      this.formId = "register";
-      this.heading = "Sign up";
-      this.authToggleText = "Already have an account?";
-      this.authToggleHref = "/?auth=login";
-      this.authToggleLink = "Log in";
-    } else {
-      this.action = "/profile/";
-      this.formId = "login";
-      this.heading = "Log in";
-      this.authToggleText = "Dont have an account?";
-      this.authToggleHref = "/?auth=register";
-      this.authToggleLink = "Sign up";
-    }
+    this.action = this.isRegisterForm
+      ? "/social/auth/register"
+      : "/social/auth/login";
+    this.formId = this.isRegisterForm ? "register" : "login";
+    this.heading = this.isRegisterForm ? "Sign up" : "Log in";
+    this.authToggleText = this.isRegisterForm
+      ? "Already have an account?"
+      : "Dont have an account?";
+    this.authToggleHref = this.isRegisterForm
+      ? "/?auth=login"
+      : "/?auth=register";
+    this.authToggleLink = this.isRegisterForm ? "Log in" : "Sign up";
   }
 
   connectedCallback() {
     this.render();
+
+    const form = this.querySelector("form");
+    form.addEventListener("submit", async (e) => {
+      try {
+        e.preventDefault();
+        const user = getFormData(e);
+        if (this.isRegisterForm) {
+          await register(user);
+        } else {
+          await login(user);
+        }
+      } catch (error) {
+        let errorMessage = this.querySelector("#auth-error");
+        if (errorMessage) {
+          errorMessage.remove();
+        }
+        errorMessage = new ErrorDialog(error, "auth-error");
+        form.insertBefore(errorMessage, this.querySelector("#container"));
+      }
+    });
   }
 
   render() {
@@ -54,9 +76,12 @@ export class AuthForm extends HTMLElement {
     const inputContainer = htmlUtilities.createHTML(
       "div",
       "grid gap-5 pb-3 pt-5",
+      null,
+      { id: "container" },
     );
 
-    if (this.query === "register") {
+    if (this.isRegisterForm) {
+      form.setAttribute("autocomplete", "off");
       const username = new InputGroup(
         "Username",
         { for: "username" },
