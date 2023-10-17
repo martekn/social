69428e927800e416a141b37ca7df9e9d2871709d
Marte <marte.mk@hotmail.com>
2023-10-17 15:17:12 +0000

    Add functionality for search page
    - Fetches data based on action query
    - Renders data based on action and search query

--- src/js/pages/search.js
+++ src/js/pages/search.js
@@ -1,10 +1,298 @@
-import { posts } from "../const/test-data/posts.js";
 import { renderPosts } from "../helper/renderPosts.js";
-import { profile } from "../const/test-data/profile.js";
-import { tags } from "../const/test-data/tags.js";
-
-renderPosts(posts, document.querySelector("#search-list"), profile);
+import Storage from "../helper/storage/index.js";
+import { requestAll } from "../helper/api/request-all.js";
+import { allPosts } from "../helper/api/request-object/all-posts.js";
+import { allUsers } from "../helper/api/request-object/all-users.js";
+import { postsByTag } from "../helper/api/request-object/posts-by-tag.js";
+import { userById } from "../helper/api/request-object/user-by-id.js";
+import { renderFilterButtons } from "../helper/render-filter-buttons.js";
+import { sortPopularPosts } from "../helper/sort-popular-posts.js";
+import { ErrorDialog } from "../components/error/error-dialog.js";
+import { renderUserSearch } from "../helper/render-user-search.js";
 
 const sidebar = document.querySelector("app-sidebar");
-sidebar.renderFollowing(profile.following);
-sidebar.renderTags(tags);
+const searchList = document.querySelector("#search-list");
+const username = Storage.get("username");
+const searchParams = new URLSearchParams(window.location.search);
+const searchQuery = searchParams.get("search") ?? "";
+let actionQuery = searchParams.get("action")?.toLowerCase() ?? "latest";
+
+const userRequests = [allUsers(), allPosts(), userById(username)];
+const postsByTagRequests = [
+  postsByTag(searchQuery),
+  allPosts(),
+  userById(username),
+];
+const allPostsRequests = [allPosts(), userById(username)];
+
+const getLatestPosts = async () => {
+  const [posts, user] = await requestAll(allPostsRequests);
+  if (posts.status === "rejected") {
+    return {
+      postsToRender: posts,
+      allPosts: posts,
+      loggedInUser: user,
+      isPosts: true,
+    };
+  }
+
+  if (!searchQuery) {
+    return {
+      postsToRender: posts,
+      allPosts: posts,
+      loggedInUser: user,
+      isPosts: true,
+    };
+  }
+  const filteredPosts = searchPosts(posts.value);
+  return {
+    postsToRender: {
+      value: filteredPosts,
+      status: posts.status,
+      response: posts.response ?? "",
+    },
+    allPosts: posts,
+    loggedInUser: user,
+    isPosts: true,
+  };
+};
+
+const getPopularPosts = async () => {
+  const [posts, user] = await requestAll(allPostsRequests);
+  if (posts.status === "rejected") {
+    return {
+      postsToRender: posts,
+      allPosts: posts,
+      loggedInUser: user,
+      isPosts: true,
+    };
+  }
+
+  posts.value = sortPopularPosts(posts.value);
+
+  if (!searchQuery) {
+    return {
+      postsToRender: posts,
+      allPosts: posts,
+      loggedInUser: user,
+      isPosts: true,
+    };
+  }
+  const filteredPosts = searchPosts(posts.value);
+  return {
+    postsToRender: {
+      value: filteredPosts,
+      status: posts.status,
+      response: posts.response ?? "",
+    },
+    allPosts: posts,
+    loggedInUser: user,
+    isPosts: true,
+  };
+};
+
+const getPostsByTags = async () => {
+  if (!searchQuery) {
+    const [posts, user] = await requestAll(allPostsRequests);
+    if (posts.status === "rejected") {
+      return {
+        postsToRender: posts,
+        allPosts: posts,
+        loggedInUser: user,
+        isPosts: true,
+      };
+    }
+
+    const filteredPosts = posts.value.filter((post) => {
+      const updatedTags = [];
+      for (const tag of post.tags) {
+        if (tag === "" || tag === "#") {
+          continue;
+        }
+        updatedTags.push(tag);
+      }
+      post.tags = updatedTags;
+
+      return post.tags.length > 0;
+    });
+
+    return {
+      postsToRender: {
+        value: filteredPosts,
+        status: posts.status,
+        response: posts.response ?? "",
+      },
+      allPosts: posts,
+      loggedInUser: user,
+      isPosts: true,
+    };
+  }
+
+  const [posts, allPosts, user] = await requestAll(postsByTagRequests);
+
+  return {
+    postsToRender: posts,
+    allPosts: allPosts,
+    loggedInUser: user,
+    isPosts: true,
+  };
+};
+
+const getUsers = async () => {
+  const [allUsers, posts, user] = await requestAll(userRequests);
+  if (allUsers.status === "rejected") {
+    return {
+      usersToRender: allUsers,
+      allPosts: posts,
+      loggedInUser: user,
+      isPosts: false,
+    };
+  }
+
+  if (!searchQuery) {
+    return {
+      usersToRender: allUsers,
+      loggedInUser: user,
+      allPosts: posts,
+      isPosts: false,
+    };
+  }
+  const filteredUsers = searchUsers(allUsers.value);
+  return {
+    usersToRender: {
+      value: filteredUsers,
+      status: allUsers.status,
+      response: allUsers.response ?? "",
+    },
+    loggedInUser: user,
+    allPosts: posts,
+    isPosts: false,
+  };
+};
+
+const getPostsWithMedia = async () => {
+  const [posts, user] = await requestAll(allPostsRequests);
+  if (posts.status === "rejected") {
+    return {
+      postsToRender: posts,
+      allPosts: posts,
+      loggedInUser: user,
+      isPosts: true,
+    };
+  }
+
+  posts.value = posts.value.filter((post) => post.media);
+
+  if (!searchQuery) {
+    return {
+      postsToRender: posts,
+      allPosts: posts,
+      loggedInUser: user,
+      isPosts: true,
+    };
+  }
+  const filteredPosts = searchPosts(posts.value);
+  return {
+    postsToRender: {
+      value: filteredPosts,
+      status: posts.status,
+      response: posts.response ?? "",
+    },
+    allPosts: posts,
+    loggedInUser: user,
+    isPosts: true,
+  };
+};
+
+/**
+ * Filters a list of posts based on a search query.
+ *
+ * @function
+ * @param {Array} posts - An array of post objects to filter.
+ * @returns {Array} - An array of filtered post objects that match the search query.
+ */
+const searchPosts = (posts) => {
+  const searchValue = searchQuery.toLowerCase();
+  const filteredPosts = posts.filter(
+    ({ author: { name }, title, body, tags }) => {
+      const nameMatch = name.toLowerCase().includes(searchValue);
+      const titleMatch = title?.toLowerCase().includes(searchValue);
+      const bodyMatch = body?.toLowerCase().includes(searchValue);
+      const tagsMatch = tags.some((tag) => tag.toLowerCase() === searchValue);
+      return nameMatch || titleMatch || bodyMatch || tagsMatch;
+    },
+  );
+
+  return filteredPosts;
+};
+
+/**
+ * Filters a list of users based on a search query.
+ *
+ * @function
+ * @param {Array} users - An array of user objects to filter.
+ * @returns {Array} - An array of filtered user objects that match the search query.
+ */
+const searchUsers = (users) => {
+  const searchValue = searchQuery.toLowerCase();
+  const filteredUsers = users.filter(({ name }) =>
+    name.toLowerCase().includes(searchValue),
+  );
+  return filteredUsers;
+};
+
+const setupSearch = async () => {
+  const fetchFunctions = {
+    latest: getLatestPosts,
+    popular: getPopularPosts,
+    tags: getPostsByTags,
+    people: getUsers,
+    media: getPostsWithMedia,
+  };
+
+  renderFilterButtons();
+
+  if (!(actionQuery in fetchFunctions)) {
+    actionQuery = "latest";
+  }
+
+  try {
+    const response = await fetchFunctions[actionQuery]();
+    sidebar.setup(response.allPosts, response.loggedInUser);
+
+    if (response.isPosts) {
+      if (
+        response.postsToRender.status === "fulfilled" &&
+        response.loggedInUser.status === "fulfilled"
+      ) {
+        renderPosts(
+          response.postsToRender.value,
+          searchList,
+          response.loggedInUser.value,
+        );
+      } else {
+        throw new Error("Failed to load posts. Please try again later.");
+      }
+    } else {
+      if (
+        response.usersToRender.status === "fulfilled" &&
+        response.loggedInUser.status === "fulfilled"
+      ) {
+        renderUserSearch(
+          response.usersToRender.value,
+          searchList,
+          response.loggedInUser.value,
+        );
+      } else {
+        throw new Error("Failed to load users. Please try again later.");
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    document
+      .querySelector("main")
+      .append(new ErrorDialog(error, "search-error"));
+  }
+};
+
+setupSearch();
