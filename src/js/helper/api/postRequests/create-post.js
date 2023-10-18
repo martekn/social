f2a1a2b1334b49590ec8b7e332f697a09231e9e2
Marte <marte.mk@hotmail.com>
2023-10-18 15:48:50 +0000

    Add POST, PUT and DELETE functions for post

---
+++ src/js/helper/api/postRequests/create-post.js
@@ -0,0 +1,27 @@
+import { request } from "../request.js";
+
+/**
+ * Creates a new social media post.
+ *
+ * @async
+ * @function
+ *
+ * @param {Object} body - The data representing the new post to be created.
+ * @param {string} body.title - The title of the post (Required).
+ * @param {string} [body.body] - The main content of the post (Optional).
+ * @param {string[]} [body.tags] - An array of tags associated with the post (Optional).
+ * @param {string} [body.media] - The URL to the media content (image or video) associated with the post (Optional).
+ *
+ * @returns {Promise<Object>} A Promise that resolves to the response object from the server.
+ */
+export const createPost = async (body) => {
+  const response = await request(
+    "/api/v1/social/posts",
+    null,
+    true,
+    "POST",
+    body,
+  );
+
+  return response;
+};
---
+++ src/js/helper/api/putRequests/update-post.js
@@ -0,0 +1,32 @@
+import { request } from "../request.js";
+
+/**
+ * Updates a social media post by its ID.
+ *
+ * @async
+ * @function
+ * @param {string|number} id - The ID of the post to be updated.
+ * @param {Object} body - The data representing the updated post.
+ * @param {string} body.title - The updated title of the post (Required).
+ * @param {string} [body.body] - The updated main content of the post (Optional).
+ * @param {string|string[]} [body.tags] - The updated tags associated with the post (Optional).
+ * @param {string} [body.media] - The updated URL to the media content (image or video) associated with the post (Optional).
+ * @returns {Promise<Object>} A Promise that resolves to the response object from the server.
+ */
+export const updatePost = async (id, body) => {
+  if (body.tags) {
+    body.tags = body.tags.trim().split(" ");
+  } else {
+    body.tags = [];
+  }
+
+  const response = await request(
+    `/api/v1/social/posts/${id}`,
+    null,
+    true,
+    "PUT",
+    body,
+  );
+
+  return response;
+};
