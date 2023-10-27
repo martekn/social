97ba3cb7e3d7efce6f221a80aa204433a4ec480c
Marte <marte.mk@hotmail.com>
2023-10-27 20:25:05 +0000

    Add scrolling functionality

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@ import Storage from "./helper/storage/index.js";
 import "./components/index.js";
 import { privatePaths } from "./const/private-paths.js";
 import { publicPaths } from "./const/public-paths.js";
+import { setSidebarScroll } from "./helper/set-sidebar-scroll.js";
+import { setNavigationScroll } from "./helper/set-navigation-scroll.js";
 
 const createPostButton = document.querySelector("#create-button");
 
@@ -20,4 +22,9 @@ if (privatePaths.find((privatePath) => path === privatePath) && !token) {
 
 if (publicPaths.find((publicPath) => path === publicPath) && token) {
   window.location.href = "/feed";
+}
+
+if (path !== "/") {
+  setSidebarScroll();
+  setNavigationScroll();
 }
