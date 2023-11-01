fde2ebd655d242d2bfc1fa13badc179678ece6b7
Marte <marte.mk@hotmail.com>
2023-11-01 17:07:57 +0000

    Validate JWT while handling public/private paths

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,8 @@ import { privatePaths } from "./const/private-paths.js";
 import { publicPaths } from "./const/public-paths.js";
 import { setSidebarScroll } from "./helper/set-sidebar-scroll.js";
 import { setNavigationScroll } from "./helper/set-navigation-scroll.js";
+import { requiredHeaderKeys, requiredPayloadKeys } from "./const/token-keys.js";
+import Jwt from "./helper/jwt/index.js";
 import Modal from "./helper/modal/index.js";
 
 const createPostButton = document.querySelector("#create-button");
@@ -14,14 +16,15 @@ if (createPostButton) {
   });
 }
 
-const token = Storage.get("accessToken");
 const path = window.location.pathname;
+const token = Storage.get("accessToken");
+const hasToken = Jwt.validate(token, requiredHeaderKeys, requiredPayloadKeys);
 
-if (privatePaths.find((privatePath) => path === privatePath) && !token) {
+if (privatePaths.find((privatePath) => path === privatePath) && !hasToken) {
   window.location.href = "/?auth=login";
 }
 
-if (publicPaths.find((publicPath) => path === publicPath) && token) {
+if (publicPaths.find((publicPath) => path === publicPath) && hasToken) {
   window.location.href = "/feed";
 }
 
